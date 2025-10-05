76c289bd0b7ce4a5c241769fd7d9a0ecaa11b794
Uyen Tran <thtuyen1011@gmail.com>
2025-10-05 12:38:56 -0700

    Update index.css with template code

---
+++ index.js
@@ -0,0 +1,9 @@
+// API Components
+const baseURL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const cohort = "/2508-FTB-ET-WEB-FT";
+const segment = "/events";
+const API = baseURL + cohort + segment;
+
+// ===== State Variables =====
+let events = [];
+let selectedEvent;
